063baa1d65419eb448f6a0160598bf65ae0e4dff
SfTd <j.zolcik@allblue.pl>
2017-12-17 15:13:51 +0100

    Development.

---
+++ allblue/src/main/java/pl/allblue/helpers/Strings.java
@@ -0,0 +1,19 @@
+package pl.allblue.helpers;
+
+public class Strings
+{
+
+    static public String EscapeLangChars(String s)
+    {
+        char[] from = new char[] { 'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż',
+                'Ą', 'Ć', 'Ę', 'Ł', 'Ń', 'Ó', 'Ś', 'Ź', 'Ż' };
+        char[] to = new char[] {'a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z',
+                'A', 'C', 'E', 'L', 'N', 'O', 'S', 'Z', 'Z' };
+
+        for (int i = 0; i < from.length; i++)
+            s = s.replace(from[i], to[i]);
+
+        return s;
+    }
+
+} 
